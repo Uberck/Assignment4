aa37044f5c34da7bed9daf654638f81ae0dd80c8
Christos Kokkalis <Uberck@gmail.com>
2020-10-07 21:11:53 -0400

    Add project files.

---
+++ Assignment4/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment4.Default" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head runat="server">
+      <title></title>
+      <style type="text/css">
+         #form1 {
+         width: 420px;
+         }
+      </style>
+   </head>
+   <body>
+      <form id="form1" runat="server">
+         <div>
+            <asp:Label ID="lblTitle" runat="server" Font-Bold="True" Text="Web Calculator using Methods"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Enter 1st value:   "></asp:Label>
+            &nbsp;
+            <asp:TextBox ID="txtNumerator" runat="server"></asp:TextBox>
+            <br />
+            <br />
+         </div>
+         <asp:Label ID="Label2" runat="server" Text="Enter 2nd value:  "></asp:Label>
+         &nbsp;
+         <asp:TextBox ID="txtDenominator" runat="server"></asp:TextBox>
+         <br />
+         <br />
+         <asp:Label ID="Label3" runat="server" Text="Select an operation using one of the buttons below."></asp:Label>
+         <p>
+            <asp:Label ID="lblResult" runat="server" Text="Result:"></asp:Label>
+         </p>
+         <asp:TextBox ID="txtResult" runat="server"></asp:TextBox>
+         <p>
+            &nbsp;
+            <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="+" Width="50px" />
+            &nbsp;&nbsp; &nbsp;
+            <asp:Button ID="btnSubtract" runat="server" OnClick="btnSubtract_Click" Text="-" Width="50px" />
+            &nbsp;
+            &nbsp;&nbsp;
+            <asp:Button ID="btnMultiply" runat="server" OnClick="btnMultiply_Click" Text="*" Width="50px" />
+            &nbsp;&nbsp; &nbsp;
+            <asp:Button ID="btnDivide" runat="server" OnClick="btnDivide_Click" Text="÷" Width="50px" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnClear" runat="server" OnClick="btnClear_Click" Text="Clear" />
+         </p>
+      </form>
+   </body>
+</html>
---
+++ Assignment4/Default.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        // Function to take input from two fields and add them
+        private int AddNumbers(int fieldOne, int fieldTwo)
+        {
+            return fieldOne + fieldTwo;
+        }
+        // Function to take input from two fields and subtract them
+        private int SubtractNumbers(int fieldOne, int fieldTwo)
+        {
+            return fieldOne - fieldTwo;
+        }
+        // Function to take input from two fields and multiply them
+        private int MultiplyNumbers(int fieldOne, int fieldTwo)
+        {
+            return fieldOne * fieldTwo;
+        }
+        // Function to take input from two fields and multiply them
+        private int DivideNumbers(int fieldOne, int fieldTwo)
+        {
+            return fieldOne / fieldTwo;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // btnAdd OnClick
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get values from text boxes
+                int fieldOne = Convert.ToInt32(txtNumerator.Text);
+                int fieldTwo = Convert.ToInt32(txtDenominator.Text);
+                // Call the AddNumbers method & print the result in the Result textbox
+                txtResult.Text = AddNumbers(fieldOne, fieldTwo) + "";
+            }
+            catch (Exception)
+            {
+                // Catch invalid input popup
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('" + "Please enter a valid number." + "');", true);
+            }
+        }
+
+        // btnSubtract OnClick
+        protected void btnSubtract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get values from text boxes
+                int fieldOne = Convert.ToInt32(txtNumerator.Text);
+                int fieldTwo = Convert.ToInt32(txtDenominator.Text);
+                // Call the SubtractNumbers method & print the result in the Result textbox
+                txtResult.Text = SubtractNumbers(fieldOne, fieldTwo) + "";
+            }
+            catch (Exception)
+            {
+                // Catch invalid input popup
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('" + "Please enter a valid number." + "');", true);
+            }
+        }
+
+        // btnMultiply OnClick
+        protected void btnMultiply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get values from text boxes
+                int fieldOne = Convert.ToInt32(txtNumerator.Text);
+                int fieldTwo = Convert.ToInt32(txtDenominator.Text);
+                // Call the SubtractNumbers method & print the result in the Result textbox
+                txtResult.Text = MultiplyNumbers(fieldOne, fieldTwo) + "";
+            }
+            catch (Exception)
+            {
+                // Catch invalid input popup
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('" + "Please enter a valid number." + "');", true);
+            }
+        }
+
+        // btnDivide OnClick
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get values from text boxes
+                int fieldOne = Convert.ToInt32(txtNumerator.Text);
+                int fieldTwo = Convert.ToInt32(txtDenominator.Text);
+                // Call the SubtractNumbers method & print the result in the Result textbox
+                txtResult.Text = DivideNumbers(fieldOne, fieldTwo) + "";
+            }
+            catch (Exception)
+            {
+                // Catch invalid input popup
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('" + "Please enter a valid number." + "');", true);
+            }
+        }
+
+        //btnClear OnClick
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumerator.Text = "";
+            txtDenominator.Text = "";
+            txtResult.Text = "";
+        }
+    }
+}
